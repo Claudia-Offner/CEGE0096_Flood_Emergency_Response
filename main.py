fd2b54066c05e45547f2364970696d7c16595873
ucesccf <72859960+ucesccf@users.noreply.github.com>
2021-01-02 13:21:56 +0000

    Added main.py (to solve for tasks) and mapper.py (to develop map features)

---
+++ main.py
@@ -0,0 +1,20 @@
+# This file is where Assignment 2 tasks will be solved.
+
+# Import necessary packages here
+
+# Add extra functions here
+
+def main():
+    """ Flood Emergency Planning """
+
+    # TASK 1: User Input
+
+    # Task 2: Highest Point Identification
+
+    # Task 3: Nearest Integrated Transport Network (ITN)
+
+    # Task 4: Shortest Path
+
+    # Task 5: Map Plotting
+
+    # Task 6: Extend the Region
---
+++ mapper.py
@@ -0,0 +1,13 @@
+# This file is where mapping and plotting code will be developed
+
+class Mapper:
+
+    def __init__(self):
+
+    # def add_raster (i.e. background, elevation)
+
+    # add add_vector (i.e. shape, roads, itn)
+
+    # def show
+
+    # def close

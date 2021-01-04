aaab0557e2b12f9454f007cdcb6e3360b410f131
ucesccf <ucesccf@ucl.ac.uk>
2021-01-04 10:52:39 +0000

    Added task 1 solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This file is where Assignment 2 tasks will be solved.
 
 # Import necessary packages here
+from shapely.geometry import Point, Polygon
 
 # Add extra functions here
 
@@ -8,6 +9,17 @@ def main():
     """ Flood Emergency Planning """
 
     # TASK 1: User Input
+
+    # Get user inputs
+    print('Please input your current location as a British National Grid coordinate')
+    x1 = 450000  # x1 = float(input('x coordinate: '))
+    y1 = 85000  # y1 = float(input('y coordinate: '))
+    # Convert inputs into Point object and elevation boundary into Polygon object
+    user_p = Point(x1, y1)
+    el_mbr = Polygon([(430000, 80000.0), (465000.0, 80000.0), (465000.0, 95000.0), (430000, 95000.0)])
+    # Get MBR
+    mbr_res = el_mbr.contains(user_p)
+    print(mbr_res)
 
     # Task 2: Highest Point Identification
 
@@ -18,3 +30,6 @@ def main():
     # Task 5: Map Plotting
 
     # Task 6: Extend the Region
+
+if __name__ == '__main__':
+    main()

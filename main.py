ac7292adf4081645b0f8390b4139cd9668b06030
Sándor István Magyari <magyari.sandor.istvan@gmail.com>
2021-01-07 11:27:02 +0000

    FEAT: solution for task1 integrated, now takes input point from that

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Import necessary packages here
 import rasterio
-from shapely.geometry import Point
+from shapely.geometry import Point, Polygon
 from pyproj import CRS
 from rasterio.plot import show
 from rasterio.mask import mask
@@ -26,9 +26,20 @@ def main():
 
     # TASK 1: User Input
 
+    # Get user inputs
+    print('Please input your current location as a British National Grid coordinate')
+    x1 = 450000  # x1 = float(input('x coordinate: '))
+    y1 = 85000  # y1 = float(input('y coordinate: '))
+    # Convert inputs into Point object and elevation boundary into Polygon object
+    user_p = Point(x1, y1)
+    el_mbr = Polygon([(430000, 80000.0), (465000.0, 80000.0), (465000.0, 95000.0), (430000, 95000.0)])
+    # Get MBR
+    mbr_res = el_mbr.contains(user_p)
+    print(mbr_res)
+
     # Task 2: Highest Point Identification
     crs = CRS.from_epsg(27700)
-    location = Point(450000, 90000)
+    location = Point(x1, y1)
     print('Creating 5km buffer around current location...')
     location_buf = location.buffer(5000)
     location_buf_gdf = gpd.GeoDataFrame({'geometry': location_buf}, index=[0], crs=crs)

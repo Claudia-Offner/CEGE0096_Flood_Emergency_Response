8fcc22a8edce526e040e1904c35ee72e4a5c3d57
Sándor István Magyari <magyari.sandor.istvan@gmail.com>
2021-01-07 11:13:24 +0000

    FEAT: print statements included, started mapper.py for task6

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import rasterio.transform
 import geopandas as gpd
 import numpy as np
 
+
 # Add extra functions here
 # https://automating-gis-processes.github.io/CSC18/lessons/L6/clipping-raster.html Time of access: 19/12/2020
 def getfeatures(gdf):
@@ -28,17 +29,20 @@ def main():
     # Task 2: Highest Point Identification
     crs = CRS.from_epsg(27700)
     location = Point(450000, 90000)
+    print('Creating 5km buffer around current location...')
     location_buf = location.buffer(5000)
     location_buf_gdf = gpd.GeoDataFrame({'geometry': location_buf}, index=[0], crs=crs)
     location_buf_coords = getfeatures(location_buf_gdf)
+    print('Reading elevation file...')
     with rasterio.open('elevation\SZ.asc') as elev:
         elev_mask, mask_transform = rasterio.mask.mask(elev, shapes=location_buf_coords, crop=True)
     # rasterio.plot.show(elev_mask, transform=mask_transform)
+    print('Searching highest point...')
     highest_point_value = np.max(elev_mask)
     highest_point_place = np.where(elev_mask[0] == highest_point_value)
     # print(elev_mask[0][highest_point_place])
     highest_point = Point(location.x - 5002.5 + highest_point_place[1][0] * 5, location.y + 5002.5 - highest_point_place[0][0] * 5)
-    # print(highest_point.xy)
+    print("The highest point's coordinates are: " + 'x=' + str(highest_point.x) + ', ' + 'y=' + str(highest_point.y))
     # Task 3: Nearest Integrated Transport Network (ITN)
 
     # Task 4: Shortest Path
@@ -46,3 +50,7 @@ def main():
     # Task 5: Map Plotting
 
     # Task 6: Extend the Region
+
+
+if __name__ == '__main__':
+    main()
